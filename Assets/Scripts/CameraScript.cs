380fa7db32ef4bce22993dd8b8e421afafcbe515
hightemplarr <hightemplarr@knights.ucf.edu>
2022-03-20 10:15:03 -0400

    Challenge commit

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -13,6 +13,14 @@ public class CameraScript : MonoBehaviour
         
     }
 
+    private void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
     // Update is called once per frame
     void LateUpdate()
     {
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,22 +10,103 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     private int scoreValue;
     public Text score;
+    public Text winMessage;
+    public float impulse;
+    private int livesValue;
+    public Text lives;
+    Animator anim;
+    private float hozMovement;
+    private float vertMovement;
+    private bool facingRight;
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+    public float JumpForce;
+    private int level;
+    public AudioSource musicSource;
+    public AudioClip musicOne;
+    public AudioClip musicTwo;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         scoreValue = 0;
+        livesValue = 3;
         score.text = "Score: " + scoreValue.ToString();
+        lives.text = "Lives: " + livesValue.ToString();
+        anim = GetComponent<Animator>();
+        facingRight = true;
+        level = 1;
+        musicSource.clip = musicOne;
+        musicSource.Play();
+        musicSource.loop = true;
     }
 
     private void Update()
     {
+        hozMovement = Input.GetAxis("Horizontal");
+        vertMovement = Input.GetAxis("Vertical");
+
         if (Input.GetKey("escape"))
         {
             Application.Quit();
         }
+
+        if (scoreValue >= 8 && level == 2)
+        {
+            winMessage.text = "You win! Game created by Nate Cowan.";
+        }
+        
+        if (scoreValue == 4 && level == 1)
+        {
+            transform.position = new Vector2(57, 0);
+            level++;
+            livesValue = 3;
+            lives.text = "Lives: " + livesValue.ToString();
+        }
+
+        if (livesValue == 0)
+        {
+            winMessage.text = "You lose. Game created by Nate Cowan.";
+            Destroy(gameObject);
+        }
+
+        if (hozMovement > 0 || hozMovement < 0)
+        {
+            if (isOnGround == true)
+            {
+                anim.SetInteger("State", 1);
+            }
+        } else if (hozMovement == 0)
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        } else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+
+        if (isOnGround == false || Input.GetKey(KeyCode.W))
+        {
+            anim.SetInteger("State", 2);
+        } else if (isOnGround == true && hozMovement == 0)
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            scoreValue = 5;
+            level = 2;
+        }
     }
+
 
     // Update is called once per frame
     void FixedUpdate()
@@ -33,25 +114,50 @@ public class PlayerScript : MonoBehaviour
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            scoreValue += 1;
+            score.text = "Score: " + scoreValue.ToString();
+            Destroy(collision.gameObject);
+            if (scoreValue == 8 && level == 2)
+            {
+                musicSource.loop = false;
+                musicSource.clip = musicTwo;
+                musicSource.Play();
+            }
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground" && isOnGround)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Coin")
+        if (collision.gameObject.tag == "Enemy")
         {
-            scoreValue += 1;
-            score.text = "Score: " + scoreValue.ToString();
-            Destroy(collision.collider.gameObject);
+            livesValue -= 1;
+            lives.text = "Lives: " + livesValue.ToString();
+            Destroy(collision.gameObject);
         }
     }
-    private void OnCollisionStay2D(Collision2D collision)
+    private void Flip()
     {
-        if(collision.collider.tag == "Ground")
-        {
-            if (Input.GetKey(KeyCode.W))
-            {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
-            }
-        }
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 }
